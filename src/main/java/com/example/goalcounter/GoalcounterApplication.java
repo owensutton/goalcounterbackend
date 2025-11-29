51344602407ff0662ef961dab930a0c2d7d656af
Owen Sutton <owensutton7@gmail.com>
2025-11-29 00:50:40 -0500

    first commit

---
+++ src/main/java/com/example/goalcounter/GoalcounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.goalcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalcounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalcounterApplication.class, args);
+	}
+
+}
